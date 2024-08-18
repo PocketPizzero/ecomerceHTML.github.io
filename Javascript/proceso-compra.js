67045f213f785b02da85c382b2ac5ad50affe7bc
PocketPizzero <fabmurillo2005@gmail.com>
2024-08-18 13:27:34 -0600

    finalizar compra incompleto

--- Javascript/proceso-compra.js
+++ Javascript/proceso-compra.js
@@ -1,3 +1,62 @@
+document.getElementById('numeroTarjeta').addEventListener('input', function() {
+	let numeroTarjeta = document.getElementById('numeroTarjeta').value
+	let fechaExpiracion = document.getElementById('fechaExpiracion').value
+	let cvv = document.getElementById('cvv').value
+  
+  //Página de BIN de bancos en Costa Rica https://bincheck.org/costa-rica?page=1
+  //BIN MASTERCARD BANCO DE COSTA RICA: 510209
+  //BIN VISA BANCO DE COSTA RICA: 410372
+  
+  // Identificar tipo de tarjeta
+	if (numeroTarjeta.length >= 6) {
+	   fetch(`https://data.handyapi.com/bin/${numeroTarjeta}`)
+	  .then(response => {
+		return response.json(); // Convertir el Response a JSON
+	  })
+	  .then(datos => {
+		console.log(datos); // Muestra el JSON en la consola
+		let imagenTarjeta = '../Images/logo - icono pestaña.jpeg';
+  
+		if (datos.Scheme === 'VISA') { 
+		  imagenTarjeta = '../Images/Proceso - Visa.png';
+		} else if (datos.Scheme === 'MASTERCARD') {
+		  imagenTarjeta = '../Images/Proceso - Mastercard.png';
+		} else {
+		  imagenTarjeta = '../Images/logo - icono pestaña.png';
+		}
+  
+		// Actualiza la fuente de la imagen
+		document.getElementById('imagenTarjeta').src = imagenTarjeta;
+  
+		// Muestra en la consola el tipo de tarjeta
+		console.log('Tipo de tarjeta:', datos.brand);
+  
+	  })
+	  .catch(error => {
+		console.error('Error:', error);
+		document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen en caso de error
+	  
+	  });
+	} else {
+	document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen si el número de tarjeta es demasiado corto
+  }
+  
+  // Validar tarjeta
+  if(!numeroTarjeta.length == 16){
+	document.querySelector('#numeroTarjeta').classList.add('is-invalid');
+	document.querySelector('.invalidad-feedback').textContent= 'Numero de tarjeta inválido';
+  }
+  /*if(fechaExpiracion){
+	document.querySelector('#cvv').classList.add('is-invalid');
+	document.querySelector('.invalidad-feedback').textContent= 'Código de seguridad inválido';
+  }*/
+  if(cvv.length!=3){
+	document.querySelector('#cvv').classList.add('is-invalid');
+	document.querySelector('.invalidad-feedback').textContent= 'Código de seguridad inválido';
+  }
+  
+  })
+
 document.getElementById('continuar').addEventListener('click', function() {
     // Simula un clic en la pestaña de medios de pago
     document.getElementById('medios-tab').classList.remove('disabled')
@@ -66,9 +125,9 @@ function mostrarFactura() {
 	var infoPago = JSON.parse(localStorage.getItem('medioPago'))
 
 	infoCarrito.forEach(function(articulo) {
-		precio = parseFloat(articulo.precio) | 0;
-		cantidad = parseInt(articulo.cantidad) | 0;
-		subtotal = parseFloat(articulo.subtotal) | 0;
+		precio = parseFloat(infoCarrito.precio) | 0;
+		cantidad = parseInt(infoCarrito.cantidad) | 0;
+		subtotal = parseFloat(infoCarrito.subtotal) | 0;
 
 		const row = document.createElement('tr')
 		row.innerHTML =
@@ -76,16 +135,16 @@ function mostrarFactura() {
             	<img src=".${articulo.imagen}" alt="" width="80">
             </td>
             <td class="info-articulo right-align">
-                ${articulo.nombre}"...X"${articulo.cantidad}"..."${articulo.subtotal}
+                ${articulo.nombre}.....x${articulo.cantidad}.....${articulo.subtotal}
             </td>`
 			
 		facturaCarrito.appendChild(row)
 	});
 
-	infoCobro.forEach(function(cobro) {
-		subtotal = parseFloat(cobro.subtotal) | 0;
-		costoEnvio = parseInt(cobro.costoEnvio) | 0;
-		total = parseFloat(cobro.total) | 0;
+
+		subtotal = parseFloat(infoCobro.subtotal) | 0;
+		costoEnvio = parseInt(infoCobro.costoEnvio) | 0;
+		total = parseFloat(infoCobro.total) | 0;
 
 		const rowEnvio = document.createElement('tr')
 		rowEnvio.innerHTML =
@@ -102,7 +161,7 @@ function mostrarFactura() {
 				Subtotal
 			</td>
 			<td class="td-subtotal right-align">
-				${costoSubtotal}
+				${subtotal}
 			</td>`
 
 		const rowTotal = document.createElement('tr')
@@ -111,17 +170,15 @@ function mostrarFactura() {
 				Total
 			</td>
 			<td class="td-total right-align">
-				${costoTotal}
+				${total}
 			</td>`
 			
 		facturaCobro.appendChild(rowEnvio)
 		facturaCobro.appendChild(rowSubtotal)
 		facturaCobro.appendChild(rowTotal)
-	});
 
-	infoPago.forEach(function(pago) {
-		precio = parseFloat(pago.precio) | 0;
-		cantidad = parseInt(pago.cantidad) | 0;
+		precio = parseFloat(infoPago.precio) | 0;
+		cantidad = parseInt(infoPago.cantidad) | 0;
 		subtotal = precio * cantidad
 
 		const rowTarjeta = document.createElement('tr')
@@ -143,6 +200,6 @@ function mostrarFactura() {
 			
 		facturaPago.appendChild(rowTarjeta)
 		facturaPago.appendChild(rowTipoEnvio)
-	});
+	
 }
 
