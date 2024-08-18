6ade3be3c5e15c40eed3794b7794b9c3ad93435a
PocketPizzero <fabmurillo2005@gmail.com>
2024-08-18 01:05:02 -0600

    tarjeta

--- Javascript/tarjeta.js
+++ Javascript/tarjeta.js
@@ -2,13 +2,14 @@ document.getElementById('numeroTarjeta').addEventListener('input', function() {
   let numeroTarjeta = document.getElementById('numeroTarjeta').value
   let fechaExpiracion = document.getElementById('fechaExpiracion').value
   let cvv = document.getElementById('cvv').value
+
 //Página de BIN de bancos en Costa Rica https://bincheck.org/costa-rica?page=1
 //BIN MASTERCARD BANCO DE COSTA RICA: 510209
 //BIN VISA BANCO DE COSTA RICA: 410372
 
 // Identificar tipo de tarjeta
   if (numeroTarjeta.length >= 6) {
-     fetch(`https://data.handyapi.com/bin/${ }`)
+     fetch(`https://data.handyapi.com/bin/${numeroTarjeta}`)
     .then(response => {
       return response.json(); // Convertir el Response a JSON
     })
