9bc9c3fd18ce0936e43fa7c095ed5b7616ca03cf
PocketPizzero <fabmurillo2005@gmail.com>
2024-08-18 15:09:18 -0600

    proceso compra

--- Javascript/proceso-compra.js
+++ Javascript/proceso-compra.js
@@ -1,136 +1,132 @@
 
-document.getElementById('numeroTarjeta').addEventListener('input', function() {
-  //Página de BIN de bancos en Costa Rica https://bincheck.org/costa-rica?page=1
-  //BIN MASTERCARD BANCO DE COSTA RICA: 510209
-  //BIN VISA BANCO DE COSTA RICA: 410372
-  const numeroTarjeta = document.getElementById('numeroTarjeta').value
+document.getElementById('numeroTarjeta').addEventListener('input', function () {
+	//Página de BIN de bancos en Costa Rica https://bincheck.org/costa-rica?page=1
+	//BIN MASTERCARD BANCO DE COSTA RICA: 510209
+	//BIN VISA BANCO DE COSTA RICA: 410372
+	const numeroTarjeta = document.getElementById('numeroTarjeta').value
 
-  // Identificar tipo de tarjeta
+	// Identificar tipo de tarjeta
 	if (numeroTarjeta.length >= 6) {
-	   fetch(`https://data.handyapi.com/bin/${numeroTarjeta}`)
-	  .then(response => {
-		return response.json(); // Convertir el Response a JSON
-	  })
-	  .then(datos => {
-		//console.log(datos); // Muestra el JSON en la consola
-		let imagenTarjeta = '../Images/logo - icono pestaña.jpeg';
-  
-		if (datos.Scheme === 'VISA') { 
-		  imagenTarjeta = '../Images/Proceso - Visa.png';
-		} else if (datos.Scheme === 'MASTERCARD') {
-		  imagenTarjeta = '../Images/Proceso - Mastercard.png';
-		} else {
-		  imagenTarjeta = '../Images/logo - icono pestaña.png';
-		}
-  
-		// Actualiza la fuente de la imagen
-		document.getElementById('imagenTarjeta').src = imagenTarjeta;
-  
-		// Muestra en la consola el tipo de tarjeta
-		console.log('Tipo de tarjeta:', datos.Scheme);
-  
-	  })
-	  .catch(error => {
-		console.error('Error:', error);
-		document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen en caso de error
-	  
-	  });
+		fetch(`https://data.handyapi.com/bin/${numeroTarjeta}`)
+			.then(response => {
+				return response.json(); // Convertir el Response a JSON
+			})
+			.then(datos => {
+				//console.log(datos); // Muestra el JSON en la consola
+				let imagenTarjeta = '../Images/logo - icono pestaña.jpeg';
+
+				if (datos.Scheme === 'VISA') {
+					imagenTarjeta = '../Images/Proceso - Visa.png';
+				} else if (datos.Scheme === 'MASTERCARD') {
+					imagenTarjeta = '../Images/Proceso - Mastercard.png';
+				} else {
+					imagenTarjeta = '../Images/logo - icono pestaña.png';
+				}
+
+				// Actualiza la fuente de la imagen
+				document.getElementById('imagenTarjeta').src = imagenTarjeta;
+
+				// Muestra en la consola el tipo de tarjeta
+				console.log('Tipo de tarjeta:', datos.Scheme);
+
+			})
+			.catch(error => {
+				console.error('Error:', error);
+				document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen en caso de error
+
+			});
 	} else {
-	document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen si el número de tarjeta es demasiado corto
-  }
-  
+		document.getElementById('imagenTarjeta').src = '../Images/logo - icono pestaña.png'; // Limpia la imagen si el número de tarjeta es demasiado corto
+	}
+
 })
 
+
+
+/* VALIDAR FORMULARIO TARJETA */
 const form = document.getElementById('cardForm');
+// Agregar un evento de envío al formulario
+form.addEventListener('submit', function (event) {
+	const numeroTarjeta = document.getElementById('numeroTarjeta').value
+	const fechaExpiracion = document.getElementById('fechaExpiracion').value
+	const cvv = document.getElementById('cvv').value
 
-	// Agregar un evento de envío al formulario
-    form.addEventListener('submit', function(event) {
-		const numeroTarjeta = document.getElementById('numeroTarjeta').value
-		const fechaExpiracion = document.getElementById('fechaExpiracion').value
-		const cvv = document.getElementById('cvv').value	
+	// Prevenir el comportamiento por defecto de envío del formulario
+	event.preventDefault();
+	// Validar tarjeta
+	if (!numeroTarjeta.length == 16) {
+		document.querySelector('#numeroTarjeta').classList.add('is-invalid');
+		document.querySelector('.invalidad-feedback').textContent = 'Numero de tarjeta inválido';
+	}
+	/*if(fechaExpiracion){
+		document.querySelector('#cvv').classList.add('is-invalid');			document.querySelector('.invalidad-feedback').textContent= 'Código de seguridad inválido';
+	}*/
+	if (cvv.length != 3) {
+		document.querySelector('#cvv').classList.add('is-invalid'); document.querySelector('.invalidad-feedback').textContent = 'Código de seguridad inválido';
+	}
+	// Verifica si el formulario es válido
+	if (form.checkValidity() === false) {            // errores de validación
+		event.stopPropagation()
+	} else {
+		// datos del formulario
+		const formData = new FormData(form);
+		const data = Object.fromEntries(formData.entries());
 
-        // Prevenir el comportamiento por defecto de envío del formulario
-        event.preventDefault();
-        
-		// Validar tarjeta
-		if(!numeroTarjeta.length == 16){
-			document.querySelector('#numeroTarjeta').classList.add('is-invalid');
-			document.querySelector('.invalidad-feedback').textContent= 'Numero de tarjeta inválido';
-		}
-		/*if(fechaExpiracion){
-			document.querySelector('#cvv').classList.add('is-invalid');
-			document.querySelector('.invalidad-feedback').textContent= 'Código de seguridad inválido';
-		}*/
-		if(cvv.length!=3){
-			document.querySelector('#cvv').classList.add('is-invalid');
-			document.querySelector('.invalidad-feedback').textContent= 'Código de seguridad inválido';
-		}
+		//  datos en la consola
+		console.log('Formulario enviado con éxito:', data);
+	}
 
-        // Verifica si el formulario es válido
-        if (form.checkValidity() === false) {
-            // errores de validación
-            event.stopPropagation();
-
-        } else {
-            // datos del formulario
-            const formData = new FormData(form);
-            const data = Object.fromEntries(formData.entries());
-
-            //  datos en la consola
-            console.log('Formulario enviado con éxito:', data);
-        }
-
-        // Agregar la clase de validación al formulario
-        form.classList.add('was-validated');
-    });
-
-document.getElementById('continuar').addEventListener('click', function() {
-    // Simula un clic en la pestaña de medios de pago
-    document.getElementById('medios-tab').classList.remove('disabled')
-    document.getElementById('medios-tab').click();
+	// Agregar la clase de validación al formulario
+	form.classList.add('was-validated');
 });
 
-document.getElementById('aceptar').addEventListener('click', function() {
+document.getElementById('continuar').addEventListener('click', function () {
+	// Simula un clic en la pestaña de medios de pago
+	document.getElementById('medios-tab').classList.remove('disabled')
+	document.getElementById('medios-tab').click();
+});
+
+document.getElementById('aceptar').addEventListener('click', function () {
 	//TARJETA DE PRUEBA: 5102091234567890
-    if(document.getElementById('cardForm').classList.contains('was-validated')){
+	if (document.getElementById('cardForm').classList.contains('was-validated')) {
 		// Simula un clic en la pestaña de medios de pago
 		document.getElementById('pago').classList.add('disabled');
 		document.getElementById('finalizar-tab').classList.remove('disabled')
 		document.getElementById('finalizar-tab').click();
 		mostrarFactura()
 	}
-	
+
 });
 
 //Esta parte es la que obtiene la informacion del carrito en consola 
 document.getElementById('continuar').addEventListener('click', () => {
-	
-	//tipo del envio
-    if (!document.querySelector('input[name="shippingOptions"]:checked').value){
-        return
 
-    }
-let tipoEnvio= document.querySelector('input[name="shippingOptions"]:checked').value;
+	//tipo del envio
+	if (!document.querySelector('input[name="shippingOptions"]:checked').value) {
+		return
+
+	}
+	let tipoEnvio = document.querySelector('input[name="shippingOptions"]:checked').value;
 
 	//total y el subtotal 
-	let subtotal = parseFloat((document.getElementById('subtotal').textContent).substring(2)) ;
-	let costoEnvio= parseFloat((document.getElementById('costoEnvio').textContent).substring(2));
-	let total= parseFloat((document.getElementById('total').textContent).substring(2)) ;
+	let subtotal = parseFloat((document.getElementById('subtotal').textContent).substring(2));
+	let costoEnvio = parseFloat((document.getElementById('costoEnvio').textContent).substring(2));
+	let total = parseFloat((document.getElementById('total').textContent).substring(2));
 
 	//consola
-	var cobro = 
+	var cobro =
 	{
-		subtotal: subtotal, 
-		costoEnvio: costoEnvio, 
-		total : total
+		subtotal: subtotal,
+		costoEnvio: costoEnvio,
+		total: total
 	}
 	localStorage.setItem('cobro', JSON.stringify(cobro))
 
-}) 
+})
 
 
 //tab de la tarjeta
-document.getElementById('pago').addEventListener('click',() => {
+document.getElementById('pago').addEventListener('click', () => {
 
 	//valores de la tarjeta
 	let numeroTarjeta = document.getElementById('numeroTarjeta').value;
@@ -138,26 +134,26 @@ document.getElementById('pago').addEventListener('click',() => {
 	let cvv = document.getElementById('cvv').value;
 
 	//consola
-	var medioPago = 
+	var medioPago =
 	{
-		numeroTarjeta: numeroTarjeta, 
-		fechaExpiracion: fechaExpiracion, 
-		cvv : cvv
+		numeroTarjeta: numeroTarjeta,
+		fechaExpiracion: fechaExpiracion,
+		cvv: cvv
 	}
 	localStorage.setItem('medioPago', JSON.stringify(medioPago))
- 
+
 })
 
 function mostrarFactura() {
 	const facturaCarrito = document.getElementById("factura-carrito")
 	const facturaCobro = document.getElementById("factura-cobro")
 	const facturaPago = document.getElementById("factura-pago")
-			
+
 	var infoCarrito = JSON.parse(localStorage.getItem('compra'))
 	var infoCobro = JSON.parse(localStorage.getItem('cobro'))
 	var infoPago = JSON.parse(localStorage.getItem('medioPago'))
 
-	infoCarrito.forEach(function(articulo) {
+	infoCarrito.forEach(function (articulo) {
 		precio = parseFloat(infoCarrito.precio) | 0;
 		cantidad = parseInt(infoCarrito.cantidad) | 0;
 		subtotal = parseFloat(infoCarrito.subtotal) | 0;
@@ -170,69 +166,69 @@ function mostrarFactura() {
             <td class="info-articulo right-align">
                 ${articulo.nombre}.....x${articulo.cantidad}.....${articulo.subtotal}
             </td>`
-			
+
 		facturaCarrito.appendChild(row)
 	});
 
 
-		subtotal = parseFloat(infoCobro.subtotal) | 0;
-		costoEnvio = parseInt(infoCobro.costoEnvio) | 0;
-		total = parseFloat(infoCobro.total) | 0;
+	subtotal = parseFloat(infoCobro.subtotal) | 0;
+	costoEnvio = parseInt(infoCobro.costoEnvio) | 0;
+	total = parseFloat(infoCobro.total) | 0;
 
-		const rowEnvio = document.createElement('tr')
-		rowEnvio.innerHTML =
-			`<td>
+	const rowEnvio = document.createElement('tr')
+	rowEnvio.innerHTML =
+		`<td>
 				Envío
 			</td>
             <td class="td-costo-envio right-align">
 				${costoEnvio}
 			</td>`
-		
-		const rowSubtotal = document.createElement('tr')
-		rowSubtotal.innerHTML =
-			`<td>
+
+	const rowSubtotal = document.createElement('tr')
+	rowSubtotal.innerHTML =
+		`<td>
 				Subtotal
 			</td>
 			<td class="td-subtotal right-align">
 				${subtotal}
 			</td>`
 
-		const rowTotal = document.createElement('tr')
-		rowTotal.innerHTML =
-			`<td>
+	const rowTotal = document.createElement('tr')
+	rowTotal.innerHTML =
+		`<td>
 				Total
 			</td>
 			<td class="td-total right-align">
 				${total}
 			</td>`
-			
-		facturaCobro.appendChild(rowEnvio)
-		facturaCobro.appendChild(rowSubtotal)
-		facturaCobro.appendChild(rowTotal)
 
-		precio = parseFloat(infoPago.precio) | 0;
-		cantidad = parseInt(infoPago.cantidad) | 0;
-		subtotal = precio * cantidad
+	facturaCobro.appendChild(rowEnvio)
+	facturaCobro.appendChild(rowSubtotal)
+	facturaCobro.appendChild(rowTotal)
 
-		const rowTarjeta = document.createElement('tr')
-		rowTarjeta.innerHTML =
-			`<td>
+	precio = parseFloat(infoPago.precio) | 0;
+	cantidad = parseInt(infoPago.cantidad) | 0;
+	subtotal = precio * cantidad
+
+	const rowTarjeta = document.createElement('tr')
+	rowTarjeta.innerHTML =
+		`<td>
 				Tarjeta
 			</td>
 			<td class="td-total right-align">
 				${pago}
 			</td>`
-		const rowTipoEnvio = document.createElement('tr')
-		rowTipoEnvio.innerHTML =
-			`<td>
+	const rowTipoEnvio = document.createElement('tr')
+	rowTipoEnvio.innerHTML =
+		`<td>
 				Tipo de envío
 			</td>
 			<td class="td-total right-align">
 				${pago}
 			</td>`
-			
-		facturaPago.appendChild(rowTarjeta)
-		facturaPago.appendChild(rowTipoEnvio)
-	
+
+	facturaPago.appendChild(rowTarjeta)
+	facturaPago.appendChild(rowTipoEnvio)
+
 }
 
