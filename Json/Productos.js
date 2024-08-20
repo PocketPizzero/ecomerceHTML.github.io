332148569cd02cb23a1d705a485f777342d726e7
PocketPizzero <fabmurillo2005@gmail.com>
2024-08-20 17:31:32 -0600

    alt json

--- Json/Productos.js
+++ Json/Productos.js
@@ -16,6 +16,7 @@ const productos =
 		"entrega_aproximada": "2 días",
 		"costo_envio": "10000",
 		"fecha_agregado": "2024-08-03",
+		"alt": "Celular Aywey May",
 		"imagenes":
 		 [
 			"./Images/Productos/Aywey_May_-_Black-removebg-preview.png",
@@ -51,7 +52,7 @@ const productos =
 			"./Images/Productos/Aywey_Tempura_-_White-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Andrea M", "★★★☆☆", "Me gustó mucho cómo la página muestra las especificaciones de cada producto de manera clara. Compré el Aywey Tempura para mi  madre y aunque es un buen celular, creo que la calidad de los materiales podría mejorar. Aun así, estoy satisfecho con la compra."],
 			["Valeria B","★★★★★","Pocketshop hizo que la compra de mi nuevo teléfono fuera sencilla y rápida. Compré un Aywey Tempura, y hasta ahora, no he tenido ningún problema. El seguimiento del pedido en la página fue muy claro y me mantuvieron informado en todo momento. "],
 			["Messi C","★☆☆☆☆","Aunque la página tiene un diseño bonito, experimenté algunos problemas al filtrar los productos. Finalmente, compré un Aywey Tempura, pero no estoy completamente satisfecho con la velocidad del dispositivo. La entrega fue rápida, pero creo que el producto podría ser mejor. "]
@@ -79,7 +80,7 @@ const productos =
 			"./Images/Productos/Aywey_Noa_-_Green-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Bernal P","★★☆☆☆","El proceso de compra en Pocketshop fue un poco confuso, ya que tuve problemas para aplicar un cupón de descuento. El Aywey Noa que compré es decente para su precio, pero tiene problemas de rendimiento al ejecutar múltiples aplicaciones."],
 			["Fabian M", "★☆☆☆☆", "El diseño de la página es atractivo, pero tuve que refrescar varias veces para que los productos se cargaran correctamente. Compré un Aywey Noa y aunque tiene buenas características, el servicio al cliente podría ser más rápido en responder."],
 			
@@ -109,7 +110,7 @@ const productos =
 			"./Images/Productos/UNIVERSE_XYZ_Flip_Colors_-_White-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Erika B","★★★★★","La página carga rápido y me gusta que las opciones de filtro faciliten la búsqueda. Compré un Universe XYZ Flip Colors y aunque es un buen teléfono, la batería podría durar un poco más. En general, una experiencia positiva."],
 			["Natalia R","★★★★★","Lo que más me gustó de Pocketshop fue la claridad de la información sobre cada producto. Terminé comprando un Universe XYZ Flip Colors, que ha sido muy bueno, especialmente en términos de rendimiento. La página también ofrece una buena experiencia de usuario, aunque podría ser un poco más rápida."],
 			["Satoru Gojo","★★☆☆☆","Tuve algunos problemas para navegar en la página de Pocketshop, ya que algunos enlaces no funcionaban. Compré un Universe XYZ Flip Colors, y aunque en general es un buen dispositivo, la cámara no cumplió con mis expectativas. Podrían mejorar la experiencia de usuario en la web."]
@@ -138,7 +139,7 @@ const productos =
 			"./Images/Productos/UNIVERSE_XYZ_Fall_-_SIlver-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Sofia L","★★★★★","La variedad de productos en Pocketshop es excelente, y los precios son competitivos. Compré un Universe XYZ Fall y estoy muy satisfecha con su rendimiento. Además, el soporte al cliente fue muy atento cuando tuve una pregunta sobre la garantía."],
 			["Sherlyn M","★★★☆☆","La página tiene un buen diseño, pero encontré algunos links rotos que me dificultaron la navegación. Compré un Universe XYZ Fall, que en general es un buen teléfono, pero la duración de la batería es inferior a lo esperado. No sé si volvería a comprarlo."],
 			
@@ -168,7 +169,7 @@ const productos =
 			"./Images/Productos/UNIVERSE_F24_-_White-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Ricardo M","★★☆☆☆", "Aunque la página es fácil de navegar, tuve problemas con el carrito de compras, que se reiniciaba cada vez que intentaba agregar un producto nuevo. Al final, compré el Universe F24, pero me decepcionó un poco su rendimiento en aplicaciones pesadas. "],
 			["Nathalie P","★★★★★", "La experiencia de compra fue fluida, y la entrega fue rápida. Adquirí un UniverseF24, y aunque es un buen dispositivo en general, noté que se calienta un poco durante las videollamadas prolongadas. A pesar de eso, estoy contenta con la compra.  "],
 			["Monkey D. Luffy","★☆☆☆☆", "Tuve problemas al intentar pagar en Pocketshop, la página se congeló varias veces. Finalmente, logré comprar un Universe F24, pero me decepcionó la calidad del sonido durante las llamadas. La página necesita mejoras en su rendimiento.  "]
@@ -197,7 +198,7 @@ const productos =
 			"./Images/Productos/Shaomay_Remid_-_Ligth_Blue-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Sebastian T","★★★☆☆","La página tiene un diseño moderno, pero me costó un poco encontrar los detalles de la garantía. Compré un Shaomay Remid  y aunque en general es un buen dispositivo, la cámara no es tan buena como esperaba. Tal vez pruebe otra marca la próxima vez. "],
 			[" Jorge L","★★☆☆☆","La experiencia de compra fue regular. La página se bloqueó varias veces durante el proceso de pago, lo que fue frustrante. Compré un Shaomay Remid y aunque tiene un diseño bonito, la batería no dura lo suficiente para un día completo de uso.  "],
 			["Goku","★★★★★","Mi experiencia en Pocketshop fue fantástica. El proceso de compra fue fluido y las opciones de pago eran claras. Compré un Shaomay Remid y estoy muy impresionado con su rendimiento. Además, la página ofrece toda la información que necesitaba para tomar una decisión informada.  "]
@@ -225,7 +226,7 @@ const productos =
 			"./Images/Productos/Shaomay_XLR8_-_Green-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Camila P","★★★★★","La experiencia de compra en Pocketshop ha sido muy buena. El proceso de pago fue rápido y sin complicaciones. Compré un Shaomay XLR8, y hasta ahora, el rendimiento ha sido impecable. Además, la entrega fue rápida y el teléfono llegó bien empaquetado. "],
 			["Karla J","★★★☆☆","Pocketshop tiene una buena selección de productos, pero tuve problemas al intentar realizar el pago con mi tarjeta de crédito. El Shaomay XLR8 que compré funciona bien, aunque la cámara podría ser mejor. El servicio al cliente tardó en resolver mi problema. "]
 			
@@ -254,7 +255,7 @@ const productos =
 			"./Images/Productos/Shaomay_Envy_-_White-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Mariano P","★★★☆☆", "La navegación en Pocketshop es generalmente buena, pero la sección de preguntas frecuentes no es muy clara. Compré un Shaomay Envy  y aunque el diseño es bonito, la pantalla es un poco frágil. Tengan cuidado con las caídas. "],
 			["Alejandro T","★☆☆☆☆", "El carrito de compras se reinició varias veces mientras navegaba, lo que fue bastante frustrante. Finalmente compré un Shaomay Envy, pero la calidad del sonido no es tan buena como esperaba. El diseño de la página es agradable, pero espero que mejoren la funcionalidad. "]
 			
@@ -283,7 +284,7 @@ const productos =
 			"./Images/Productos/Peraphone_13_-_Green-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Daniel T","★★☆☆☆","La página se ve bien, pero es un poco lenta al cargar las imágenes de los productos. Compré un Peraphone 13  y, aunque las características prometían mucho, me encontré con que la batería se agota rápido. No fue la mejor experiencia de compra. "],
 			["Brandon S","★★★★★","La variedad de productos en Pocketshop es impresionante, y las descripciones detalladas me ayudaron a decidirme por el Peraphone 13. El teléfono es rápido y tiene una pantalla increíble. El proceso de pago fue sencillo y el envío llegó a tiempo."],
 			["Sukuna","★★★☆☆","La página tiene potencial, pero experimenté algunos errores al intentar agregar productos al carrito. Finalmente, compré un Peraphone 13, pero me decepcionó la duración de la batería. Espero que mejoren la funcionalidad de la página. "] 
@@ -312,7 +313,7 @@ const productos =
 			"./Images/Productos/Peraphone_14_-_Cyan-removebg-preview.png"
 		],
 		"reseñas":
-		 [
+		[
 			["Jeremy B","★★★★★","Me encantó la facilidad para comparar productos en Pocketshop. Terminé comprando un Peraphone 14, que ha sido perfecto para mis necesidades diarias. La página también ofrece información clara sobre las garantías, lo cual es un gran plus. "],
 			["Neymar J","★★★★★","El diseño de Pocketshop es intuitivo y moderno. Compré un Peraphone 14, que ha funcionado de maravilla. La batería dura bastante, y la calidad de la cámara es excelente. Aunque tuve una duda sobre la garantía, el servicio al cliente fue muy atento y resolvieron mi problema rápidamente. "],
 			["Tanjiro","★★★☆☆","Lo que más me gustó de Pocketshop fue la transparencia en la información sobre cada producto. Compré un Peraphone 14, y la calidad del dispositivo es muy buena, especialmente en lo que respecta a la cámara. Sin embargo, la página podría ofrecer más opciones de personalización al filtrar productos. "]
@@ -344,7 +345,7 @@ const productos =
 			 
 		],
 		"reseñas":
-		 [
+		[
 			["Fiorella C","★★★★★", "La interfaz de Pocketshop es muy intuitiva y encontré lo que buscaba rápidamente. Compré un Peraphone 15, y la calidad de la cámara es muy buena. Estoy muy satisfecho con mi compra y con la rapidez del envío. "],
 			["Diego G","★★★☆☆","La página tiene un diseño atractivo, pero noté que algunas imágenes de los productos tardan en cargar. Compré un Peraphone X15  y aunque en general es un buen teléfono, la pantallapodría ser mejor. No estoy completamente satisfecho con la compra. "],
 			["Cristiano R","★★★★★","Todo el proceso de compra en Pocketshop fue muy fácil. Me gustó cómo la página permite comparar productos, lo que me ayudó a elegir el Peraphone 15.  Es un dispositivo excelente, con una pantalla nítida y un rendimiento fluido. Definitivamente compraría aquí otra vez."]
