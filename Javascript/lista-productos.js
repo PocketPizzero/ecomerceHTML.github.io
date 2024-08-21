f1ebb87c18f33b9b55dc0bef234997336094ba3d
PocketPizzero <fabmurillo2005@gmail.com>
2024-08-20 18:44:37 -0600

    COMMIT

--- Javascript/lista-productos.js
+++ Javascript/lista-productos.js
@@ -103,3 +103,93 @@ function mostrarProductos(categoria){
         })
     }
 }
+
+function mostrarProductosAlmacenamiento(almacenamiento){
+    // Limpiar
+    document.getElementById("lista-productos").innerHTML = ""
+
+        // Filtrar por categoria
+        const productosFiltrados = productos.filter(producto => producto.almacenamiento === almacenamiento)
+
+        // Crear espacio de la categoria
+        const divCategoria = document.createElement("div")
+        divCategoria.classList.add("categoria")
+        divCategoria.classList.add("mb-5")
+
+        divCategoria.innerHTML =
+        `
+            <h2 class="text-center">${almacenamiento}</h2>
+            <div class="row justify-content-center mx-4">
+            
+            </div>
+        `
+        document.getElementById("lista-productos").appendChild(divCategoria)
+        
+        // Agregar productos fltrados
+        productosFiltrados.forEach(producto => {
+
+            const colProducto = document.createElement("div")
+            colProducto.classList.add("col-md-4")
+            colProducto.classList.add("mb-4")
+                const cardProducto =
+                `<div class="card mb-5 fs-1 slideDown">
+                    <img src=".${producto.imagenes[0]}" class="card-img-top" alt="${producto.alt}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">&#8353; ${producto.precio}</p>
+                        <button type="button" class="btn btn-primary btn-lg" onclick="detalleProducto(${producto.id})">
+                            Más Información
+                        </button>             
+                    </div>
+                </div>`
+                    
+            colProducto.innerHTML = cardProducto
+            divCategoria.children[1].appendChild(colProducto)
+        })
+    
+}
+
+function mostrarProductosRam(ram){
+    // Limpiar
+    document.getElementById("lista-productos").innerHTML = ""
+
+        // Filtrar por categoria
+        const productosFiltrados = productos.filter(producto => producto.RAM === ram)
+
+        // Crear espacio de la categoria
+        const divCategoria = document.createElement("div")
+        divCategoria.classList.add("categoria")
+        divCategoria.classList.add("mb-5")
+
+        divCategoria.innerHTML =
+        `
+            <h2 class="text-center">${ram}</h2>
+            <div class="row justify-content-center mx-4">
+            
+            </div>
+        `
+        document.getElementById("lista-productos").appendChild(divCategoria)
+        
+        // Agregar productos fltrados
+        productosFiltrados.forEach(producto => {
+
+            const colProducto = document.createElement("div")
+            colProducto.classList.add("col-md-4")
+            colProducto.classList.add("mb-4")
+                const cardProducto =
+                `<div class="card mb-5 fs-1 slideDown">
+                    <img src=".${producto.imagenes[0]}" class="card-img-top" alt="${producto.alt}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">&#8353; ${producto.precio}</p>
+                        <button type="button" class="btn btn-primary btn-lg" onclick="detalleProducto(${producto.id})">
+                            Más Información
+                        </button>             
+                    </div>
+                </div>`
+                    
+            colProducto.innerHTML = cardProducto
+            divCategoria.children[1].appendChild(colProducto)
+        })
+    
+}
